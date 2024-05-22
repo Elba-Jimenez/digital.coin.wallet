64aea68e288e6b96159b659acca4c6d697770af8
Elba-Jimenez <115684363+Elba-Jimenez@users.noreply.github.com>
2024-05-21 20:17:42 -0400

    Update Firebaseconfig.js

--- Firebaseconfig.js
+++ Firebaseconfig.js
@@ -1,19 +1,17 @@
-// firebaseConfig.js
-import firebase from '@react-native-firebase/app';
-import '@react-native-firebase/auth';
-import '@react-native-firebase/firestore';
+// Import Firebase modules and configuration
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
+// Initialize Firebase with your configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBMS7hdH3a-s9rwUpku-ytYnvkN6qMLRKw",
+  authDomain: "digital-coin-wallet.firebaseapp.com",
+  projectId: "digital-coin-wallet",
+  storageBucket: "digital-coin-wallet.appspot.com",
+  messagingSenderId: "155591819246",
+  appId: "1:155591819246:web:60e349eab9887ca1bdd8ff",
+  measurementId: "G-GZRK9LYN48"
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export { firebase };
+firebase.initializeApp(firebaseConfig);
